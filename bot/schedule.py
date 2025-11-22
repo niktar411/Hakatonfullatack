7166527ff6e04e883f5edf8684a0ede377f1f043
tamalati <rusteamplay@gmail.com>
2025-11-22 23:18:09 +0500

    ebat

--- bot/schedule.py
+++ bot/schedule.py
@@ -14,12 +14,23 @@ async def check_and_send_notifications(bot: Bot):
             date = entry.timestamp
             now = datetime.now()
             if (date < now and (date + timedelta(seconds=60)) > now):
+                action = entry.action
+                subject = entry.data.subject 
+                day = entry.details.day
                 group = entry.details.group
                 building = entry.details.building
-                day = entry.details.day
-                lesson = entry.details.lessonNumber
+                number = entry.details.lessonNumber
+                room = entry.data.room
+                teacher = entry.data.teacher
 
-            
+                if action == "DELETE_LESSON":  
+                    users = await get_users(group_name=group)
+                    for user in users:
+                        bot.send_message(chat_id=user.user_id, text=f"Предмет {subject} который пройдет в {day} убран из расписания!")
+                if action == "UPDATE_LESSON":
+                    users = await get_users(group_name=group)
+                    for user in users:
+                        bot.send_message(chat_id=user.user_id, text=f"Предмет {subject} ")
 
-
+                
     pass
